159659663da8f84324b1d2827a113e343159274d
langkitang04 <2010314014@mahasiswa.upnvj.ac.id>
2022-07-23 13:14:56 +0700

    changing to gps++

---
+++ gps_track/gps_track.ino
@@ -0,0 +1,26 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+SoftwareSerial serial_gps(D2, D1); 
+TinyGPSPlus gps;
+double latitude, longitude;
+
+void setup() {
+  Serial.begin(9600);
+  serial_gps.begin(9600);
+
+}
+
+void loop() {
+  
+  while(serial_gps.available()) {
+    gps.encode(serial_gps.read());
+  }
+  if(gps.location.isUpdated()) {
+    latitude = gps.location.lat();
+    longitude = gps.location.lng();
+    String link = "www.google.com/maps/place/" + String(latitude) + "," + String(longitude) ;
+    Serial.println(link);
+    delay(500);
+  }
+  
+}
